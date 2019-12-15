d1f4007a01c77f2e6f887f74f0bd04205dc86a87
AndresUPS93 <andy_l3a@hotmail.com>
2019-12-15 10:59:37 -0500

    Subiendo el proyecto java a GitHub

---
+++ src/Beans/Pessoa.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+/**
+ *
+ * @author plocabral
+ */
+public class Pessoa {
+
+    private int cpf;
+    private int rg;
+    private int idade;
+    private String nome;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(int cpf, int rg, int idade, String nome) {
+        this.cpf = cpf;
+        this.rg = rg;
+        this.idade = idade;
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getRg() {
+        return rg;
+    }
+
+    public void setRg(int rg) {
+        this.rg = rg;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" + "cpf=" + cpf + ", rg=" + rg + ", idade=" + idade + ", nome=" + nome + '}';
+    }
+
+}
